8e1c320b83bb878abe5cf2a24dc76808d2551e02
s1951693 <s1951693@ed.ac.uk>
2022-11-15 03:09:33 +0000

    Login Main Page with register

--- app/src/main/java/com/specknet/pdiotapp/MainActivity.java
+++ app/src/main/java/com/specknet/pdiotapp/MainActivity.java
@@ -2,19 +2,31 @@ package com.specknet.pdiotapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.specknet.pdiotapp.Login.LoginActivity;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+//import com.specknet.pdiotapp.Login.LoginActivity;
 import com.specknet.pdiotapp.Login.RegisterActivity;
+import com.specknet.pdiotapp.Login.StartActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button register;
     private Button login;
+    private EditText email;
+    private EditText password;
 
+    private FirebaseAuth auth;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
 
         register = findViewById(R.id.register);
         login = findViewById(R.id.login);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        auth = FirebaseAuth.getInstance();
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +50,20 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+                loginUser(txt_email, txt_password);
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, StartActivity.class);
                 startActivity(intent);
                 finish();
             }
